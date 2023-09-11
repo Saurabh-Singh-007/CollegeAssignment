2cc7fc37447cfed048e46d553d49b9bd17da89d8
Saurabh Singh <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-09-11 14:35:27 +0530

    completed 3c

--- assignment4/q3Polynomials.py
+++ assignment4/q3Polynomials.py
@@ -1,14 +1,14 @@
-# ls=eval(input("Enter the list :"))
-# n=int(input("Enter the degree"))
-# def coefficient(n):
-#     return ls[n-1]
-# print(coefficient(n))
+ls=eval(input("Enter the list :"))
+n=int(input("Enter the degree"))
+def coefficient(n):
+    return ls[n-1]
+print(coefficient(n))
 
 
 
-# def function (x):
-#     return 4*(x**3)-6*(x**2)-1
-# print(function(int(input("Enter the number x :"))))
+def function (x):
+    return 4*(x**3)-6*(x**2)-1
+print(function(int(input("Enter the number x :"))))
 
 
 
@@ -24,23 +24,31 @@ for j in range (0,n2+1):
 #adds polynomials
 l=[]
 if (n1>n2):
-    for i in range (0,n1-n2):
+    for i in range (n1,n1-n2-1,-1):
         l.append(ls_1[i])
-    k=0
-    for j in range (n1-n2,n1+1):
-        l.append(ls_1[j]+ls_2[k])
-        k+=1
+   
+    for j in range (n1-n2-1,-1,-1):
+        l.append(ls_1[j]+ls_2[j])
+        
 elif(n2>n1):
-    for l in range (0,n2-n1):
-        l.append(ls_1[l])
-    m=0
-    for n in range (n2-n1,n2+1):
-        l.append(ls_1[n]+ls_2[m])
-        m+=1
+    for m in range (n2,n2-n1-1,-1):
+        l.append(ls_2[m])
+
+    for n in range (n2-n1,-1,-1):
+        l.append(ls_1[n]+ls_2[n])
+        
 else:
-    for o in range (0,n1):
+    for o in range (0,n1+1):
         l.append (ls_1[o]+ls_2[o])
-print(l)
+
+l=l[::-1]
+
+for k in range (0,len(l)):
+    print("("+str(l[k])+"x^"+str(k)+")"+"+",end="")
+
+
+
+
 
 
              
