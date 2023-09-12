a9a77f0a6d244b57518df2d04da28b0da25f9693
Saurabh Singh <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-09-12 12:18:22 +0530

    completed bonus 1

--- assignment4/bonus_1.py
+++ assignment4/bonus_1.py
@@ -1,12 +1,14 @@
-from numpy import roots as r 
+from numpy import roots as r, round , real_if_close
 n=int(input("Enter the degree of polynomial"))
 ls=[]
-for i in range (0,n+1):
+for i in range (n,-1,-1):
     ls.append(int(input("Enter the coefficient of x^"+str(i))))
 l=r(ls)
-print(l)
+lis=[]
+
 for element in l:
-    element
+    lis.append((round((element))))
+print(lis)
 
 
 
