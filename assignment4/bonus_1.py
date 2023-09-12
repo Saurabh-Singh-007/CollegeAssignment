71597a250071f586de2e3b2a2282b6988cba70a0
Saurabh Singh <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-09-12 12:05:38 +0530

    bonus1

---
+++ assignment4/bonus_1.py
@@ -0,0 +1,12 @@
+from numpy import roots as r 
+n=int(input("Enter the degree of polynomial"))
+ls=[]
+for i in range (0,n+1):
+    ls.append(int(input("Enter the coefficient of x^"+str(i))))
+l=r(ls)
+print(l)
+for element in l:
+    element
+
+
+
