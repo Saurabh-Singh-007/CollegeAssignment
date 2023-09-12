d913876d1b0d26a48984830f7068886250540d97
Saurabh Singh <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-09-12 11:34:35 +0530

    done 4b.

--- assignment4/q4_2Dlists.py
+++ assignment4/q4_2Dlists.py
@@ -1,3 +1,35 @@
+# rows=int(input("Enter the number of rows"))
+# columns=int(input("Enter the number of columns"))
+# matrix=[]
+# for i in range(rows):
+#     row=[]
+#     for j in range (columns):
+#         row.append(input("Enter the element for ("+str(i+1)+","+str(j+1)+")"))
+#     matrix.append(row)
+
+# for m in matrix:
+#     print(m)
+
+# k=int(input("Enter the number you want to search :"))
+# rowIndex=-1
+# columnIndex=-1
+# for n in range(0,len(matrix)):
+#     for o in range(0,len(matrix[n])):
+#         if(matrix[n][o]==str(k)):
+#             rowIndex=n
+#             columnIndex=o
+#             break
+
+
+# if(rowIndex!=-1) :      
+#     print("Found at (row,column) = ("+str(n+1)+","+str(o+1)+")")
+# else:
+#     print("Not found")
+
+
+
+import math
+
 rows=int(input("Enter the number of rows"))
 columns=int(input("Enter the number of columns"))
 matrix=[]
@@ -7,24 +39,29 @@ for i in range(rows):
         row.append(input("Enter the element for ("+str(i+1)+","+str(j+1)+")"))
     matrix.append(row)
 
-for m in matrix:
-    print(m)
 
-k=int(input("Enter the number you want to search :"))
-rowIndex=-1
-columnIndex=-1
-for n in range(0,len(matrix)):
-    for o in range(0,len(matrix[n])):
-        if(matrix[n][o]==str(k)):
-            rowIndex=n
-            columnIndex=o
-            break
+if rows==columns:
+    n=rows
+    count=0
+    count_s=0
+    for i in range(0,rows):
+        for j in range(0,columns):
+            if matrix[i][j]==matrix[j][i]:
+                count+=1
+    if count ==math.pow(n,2):
+        print("It is symmetric matrix")
+    else:
+        print("It is not a symmetric matrix")
+    
+    for k in range(0,rows):
+        for l in range(0,columns):
+            if int(matrix[i][j])==int((int(matrix[j][i]))*(-1)):
+                count_s+=1
+    if count_s==math.pow(n,2):
+        print("It is a skew symmetric matrix")
+    else:
+        print("It is not a skew symmetric matrix")
 
-
-if(rowIndex!=-1) :      
-    print("Found at (row,column) = ("+str(n+1)+","+str(o+1)+")")
-else:
-    print("Not found")
 
             
 
