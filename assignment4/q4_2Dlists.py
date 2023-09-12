19f2d857341857c998bdb29f6f4c8a98b34dfdaf
Saurabh Singh <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-09-12 13:48:57 +0530

    final

--- assignment4/q4_2Dlists.py
+++ assignment4/q4_2Dlists.py
@@ -1,30 +1,30 @@
-# rows=int(input("Enter the number of rows"))
-# columns=int(input("Enter the number of columns"))
-# matrix=[]
-# for i in range(rows):
-#     row=[]
-#     for j in range (columns):
-#         row.append(input("Enter the element for ("+str(i+1)+","+str(j+1)+")"))
-#     matrix.append(row)
+rows=int(input("Enter the number of rows"))
+columns=int(input("Enter the number of columns"))
+matrix=[]
+for i in range(rows):
+    row=[]
+    for j in range (columns):
+        row.append(input("Enter the element for ("+str(i+1)+","+str(j+1)+")"))
+    matrix.append(row)
 
-# for m in matrix:
-#     print(m)
+for m in matrix:
+    print(m)
 
-# k=int(input("Enter the number you want to search :"))
-# rowIndex=-1
-# columnIndex=-1
-# for n in range(0,len(matrix)):
-#     for o in range(0,len(matrix[n])):
-#         if(matrix[n][o]==str(k)):
-#             rowIndex=n
-#             columnIndex=o
-#             break
+k=int(input("Enter the number you want to search :"))
+rowIndex=-1
+columnIndex=-1
+for n in range(0,len(matrix)):
+    for o in range(0,len(matrix[n])):
+        if(matrix[n][o]==str(k)):
+            rowIndex=n
+            columnIndex=o
+            break
 
 
-# if(rowIndex!=-1) :      
-#     print("Found at (row,column) = ("+str(n+1)+","+str(o+1)+")")
-# else:
-#     print("Not found")
+if(rowIndex!=-1) :      
+    print("Found at (row,column) = ("+str(n+1)+","+str(o+1)+")")
+else:
+    print("Not found")
 
 
 
