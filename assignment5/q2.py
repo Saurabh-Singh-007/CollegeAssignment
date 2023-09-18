271731bcb43818a9e5430cca0859f0e8dd3281b6
Saurabh-Singh-007 <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-09-18 11:20:43 +0530

    q2 done

---
+++ assignment5/q2.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+
+
+def f(n):
+    if n<2:
+        return 1
+    else:
+        return 1.65*f(n-1)
+    
+
+def g(n):
+    if n<2:
+        return 1
+    else:
+        return g(n-1)+g(n-2)
+    
+
+def h(n):
+    if n<2:
+        return 2
+    else:
+        return 2*h(n-2)
+    
+
+def k(n):
+    if n<3:
+        return 3
+    else:
+        return k(n-1)+k(n-3)
+    
+
+x=[]
+y=[]
+z=[]
+o=[]
+j=[]
+for i in range(0,50):
+    x.append(f(i))
+    y.append(g(i))
+    z.append(h(i))
+    o.append(k(i))
+    j.append(i)
+
+
+plt.scatter(j,x,color='red',label='f(n)')
+plt.show()
+
+plt.scatter(j,y,color='blue',label='g(n)')
+plt.show()
+
+plt.scatter(j,z,color='green',label='h(n)')
+plt.show()
+
+plt.scatter(j,o,color='black',label='k(n)')
+plt.show()
+
+
+
+
+plt.scatter(j,x,color='red',label='f(n)')
+
+
+plt.scatter(j,y,color='blue',label='g(n)')
+
+
+plt.scatter(j,z,color='green',label='h(n)')
+
+
+plt.scatter(j,o,color='black',label='k(n)')
+plt.legend()
+plt.show()
+
+
+print("f(n) grows exponentially")
+print("g(n) is fibonacci sequence")
+print("h(n) doubles for even n while remaining constant for odd n")
+print("k(n) is a sequence that adds the previous 2 odd numbers to the previous even number")
+
