a553028bb50e56beee983d4370a8fe03231ca7ca
Saurabh-Singh-007 <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-10-02 12:44:05 +0530

    bonus question

---
+++ assignment5/Bonus.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# Use pandas to read the CSV
+csvData = pd.read_csv('Data.csv', sep=',')
+
+# Convert the pandas dataframe into a NumPy array
+csvDataNum = csvData[['State', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']].to_numpy()
+
+# Convert the NumPy array into a list of lists
+data = csvDataNum.tolist()
+PercentFarmerTakingLoans=[]
+for i in range(len(data)):
+    PercentFarmerTakingLoans.append(data[i][2])
+
+
+perCapitaIncome=[]
+for i in range(len(data)):
+    perCapitaIncome.append(data[i][9])
+
+MeanX=sum(PercentFarmerTakingLoans)/len(PercentFarmerTakingLoans)
+MeanY=sum(perCapitaIncome)/len(perCapitaIncome)
+
+#Calculate Pearson Correlation Coefficient
+numerator=0
+denominator1=0
+denominator2=0
+for i in range(len(data)):
+    numerator+=(PercentFarmerTakingLoans[i]-MeanX)*(perCapitaIncome[i]-MeanY)
+    denominator1+=(PercentFarmerTakingLoans[i]-MeanX)**2
+    denominator2+=(perCapitaIncome[i]-MeanY)**2
+r=numerator/((denominator1*denominator2)**0.5)
+print("Pearson Correlation Coefficient is: ",r)
+
+
