a7d8b5d424b0ef00375ca3bc178b266db88aeb25
Saurabh-Singh-007 <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-10-04 14:23:42 +0530

    part c

--- assignment5/q1.py
+++ assignment5/q1.py
@@ -11,7 +11,6 @@ csvDataNum = csvData[['State', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 # Convert the NumPy array into a list of lists
 data = csvDataNum.tolist()
 
-print(data)
 #Function to calculate population density
 def PopDensity(data):
     pop=[]
@@ -45,8 +44,12 @@ def women_in_workforce(data):
     print("Minimum percentage of women in workforce are: ",min(women_in_workforce))
     print("Average percentage of women in workforce are: ",sum(women_in_workforce)/len(women_in_workforce))
     print("Median percentage of women in workforce are: ",sorted(women_in_workforce)[len(women_in_workforce)//2])
-    print("Mode percentage of women in workforce are: "+max((women_in_workforce),key=women_in_workforce.count))
+    print("Mode percentage of women in workforce are: ",max((women_in_workforce),key=women_in_workforce.count))
     return women_in_workforce 
+
+PopDensity(data)
+MarginalFarmers(data)
+women_in_workforce(data)
 
 
 x=[]
@@ -64,6 +67,28 @@ plt.bar(z,x,width=width,color='black',label="Percenage Area with >30 percent slo
 plt.bar(p1,y,width=width,color='red',label="Road Density*100")
 plt.xlabel('States',fontsize=14)
 plt.xticks(rotation=40)
+plt.legend()
+plt.show()
+
+perArea30=[]
+roadDen=[]
+states=[]
+for i in data:
+    perArea30.append(i[4])      #loop for adding values to the following lists
+
+perArea30.sort()
+for i in perArea30:
+    for j in data:
+        if i in j:
+            roadDen.append(j[5])
+            states.append(j[0])
+
+xValues=np.arange(len(roadDen))
+plt.bar(xValues,roadDen,width=0.4,label='road density')
+plt.xticks(xValues,states,rotation='vertical')              #rotating the x-axis labels by 90 degrees and setting the x-axis labels
+plt.xlabel('States',fontsize=24)
+plt.ylabel('road density',fontsize=24)
+plt.title('Road density in increasing order of percentage area with slope > 30%',fontsize=24)
 plt.legend()
 plt.show()
 
