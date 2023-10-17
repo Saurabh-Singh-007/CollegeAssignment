e0d4acdec21f9128bd88778cac6fce1db01bf3d9
Saurabh-Singh-007 <144089352+Saurabh-Singh-007@users.noreply.github.com>
2023-10-18 01:05:09 +0530

    2 changes

--- assignment6/problem2.py
+++ assignment6/problem2.py
@@ -2,7 +2,7 @@ import sys
 import csv
 import re 
 
-def clean_text(text):
+def clean_text(text):  # function to clean the text
     # Remove special characters and split into words
     cleaned_text = re.sub(r'[^a-zA-Z\s\']', '', text)
     words = cleaned_text.split()
@@ -17,34 +17,34 @@ input_file=sys.argv[1]
 
 try:
     with open (input_file) as file:
-        text=file.read()
+        text=(file.read()).lower()     # read the entire file as a single string
 
-except FileNotFoundError:
-    print(f"File {input_file} does not exist ")
+except FileNotFoundError or Exception:
+    print(f"File {input_file} does not exist or cannot be opened.")
     sys.exit()
 
 
 words=clean_text(text)
 word_count={}
 
-for word in words:
+for word in words:        # count the frequency of each word
     if word in word_count:
         word_count[word]+=1
     else:
-        word_count[word]=1
+        word_count[word]=1     # add the word to the dictionary
 
-def frequency(word):
+def frequency(word):      # function to sort the dictionary based on the frequency
     return word[1]
 
-sorted_word_count=dict(sorted(word_count.items(), key = frequency,reverse=True ))
+sorted_word_count=dict(sorted(word_count.items(), key = frequency,reverse=True ))  # sort the dictionary based on the frequency
 
 output_file = 'problem2Output.csv'
 
 with open(output_file, 'w', newline='') as csvop:
-    writerObj = csv.DictWriter(csvop, fieldnames=['word', 'frequency'])
+    writerObj = csv.DictWriter(csvop, fieldnames=['word', 'frequency'])   # write the sorted dictionary to a csv file
     writerObj.writeheader()
     for word, frequency in sorted_word_count.items():
-        writerObj.writerow({'word': word, 'frequency': frequency})
+        writerObj.writerow({'word': word, 'frequency': frequency})   
 
 print(f"Word frequencies have been written to {output_file}")
 
